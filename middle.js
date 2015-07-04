f5e82ea626dbb79420fd06240f05dc55e49f96e0
YukiTETSUKA <g031k117@s.iwate-up.ac.jp>
2015-07-04 12:59:20 +0000

    物体の初期配置を行う関数の追加，変数名の変更

--- middle.js
+++ middle.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
   var count = 0;
   
-  $('#target').click(function(){
-    $(this).css('display', 'none');
-    var x = getRandomInt(0, window.innerWidth  - parseInt($(this).css('width') , 10));
-    var y = getRandomInt(0, window.innerHeight - parseInt($(this).css('height'), 10));
-    $(this).css({top: y, left: x, display: 'block'});
-  });
-  
   function getRandomInt(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+  
+  /* 初期化 */
+  var initTargetPosition = function(){
+    var top  = getRandomInt(0, window.innerHeight - parseInt($('#target').css('height'), 10));
+    var left = getRandomInt(0, window.innerWidth  - parseInt($('#target').css('width') , 10));
+    $('#target').css({top: top, left: left});
+  };
+  
+  initTargetPosition();
+  
+  $('#target').click(function(){
+    $(this).css({display: 'none'});
+    var top  = getRandomInt(0, window.innerHeight - parseInt($(this).css('height'), 10));
+    var left = getRandomInt(0, window.innerWidth  - parseInt($(this).css('width') , 10));
+    console.log('top:', top, ' left:', left);
+    $(this).css({top: top, left: left, display: 'block'});
+  });
 });
