108727aa7c60095d84dd10b01aa693b5dda7c56f
YukiTETSUKA <g031k117@s.iwate-up.ac.jp>
2015-07-05 07:28:43 +0000

    変数名の変更，コメントの追加

--- middle.js
+++ middle.js
@@ -1,24 +1,45 @@
 $(document).ready(function(){
-  var count = 0;
-  
-  function getRandomInt(min, max){
+  var timeLimit = 5; // 制限時間(単位：秒)
+  var count = 0; // 経過時間
+  var score = 0;
+  var getRandomInt = function(min, max){ // min~maxの乱数を発生
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  };
+  var init = function(){ // 物体の初期配置，タイマーの設置等
+    var destinationTop  = getRandomInt(0, window.innerHeight - parseInt($('#target').css('height'), 10));
+    var destinationLeft = getRandomInt(0, window.innerWidth  - parseInt($('#target').css('width') , 10));
+    $('#target').css({top: destinationTop, left: destinationLeft});
+    $('#timer').text('残り' + timeLimit + '秒');
+    $('#timer').css({color: 'black'});
+  };
+    
+  init();
   
-  /* 初期化 */
-  var initTargetPosition = function(){
-    var top  = getRandomInt(0, window.innerHeight - parseInt($('#target').css('height'), 10));
-    var left = getRandomInt(0, window.innerWidth  - parseInt($('#target').css('width') , 10));
-    $('#target').css({top: top, left: left});
+  // 物体が押された場合，スコアを加算し，ランダムに再配置
+  $('#target').click(function(){
+    score++;
+    $(this).css({display: 'none'});
+    var destinationTop  = getRandomInt(0, window.innerHeight - parseInt($(this).css('height'), 10));
+    var destinationLeft = getRandomInt(0, window.innerWidth  - parseInt($(this).css('width') , 10));
+    $(this).css({top: destinationTop, left: destinationLeft, display: 'block'});
+  });
+  
+  // 結果をアラートで出力
+  var displayResult = function(score){
+    alert('あなたのスコアは' + score + '点です');
   };
   
-  initTargetPosition();
+  var countDownIntervalId = setInterval(function(){
+    if (timeLimit > count) { // 制限時間内であればカウントダウン継続
+      $('#timer').html('残り' + (timeLimit - (++count)) + '秒');
+      if (timeLimit - count <= 3) {
+        $('#timer').css({color: 'red'});
+      }
+    } else { // 時間切れの場合結果を表示
+      clearInterval(countDownIntervalId);
+      $('#target').css({display: 'none'});
+      displayResult(score);
+    }
+  }, 1000);
   
-  $('#target').click(function(){
-    $(this).css({display: 'none'});
-    var top  = getRandomInt(0, window.innerHeight - parseInt($(this).css('height'), 10));
-    var left = getRandomInt(0, window.innerWidth  - parseInt($(this).css('width') , 10));
-    console.log('top:', top, ' left:', left);
-    $(this).css({top: top, left: left, display: 'block'});
-  });
 });
