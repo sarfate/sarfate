34e18db5224916325b51cb616ec671a5421f55f2
YukiTETSUKA <g031k117@s.iwate-up.ac.jp>
2015-07-18 03:03:14 +0000

    物体を動かした

--- middle.js
+++ middle.js
@@ -5,23 +5,26 @@ $(document).ready(function(){
   var getRandomInt = function(min, max){ // min~maxの乱数を返却
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
+  var moveObject = function(){ // 物体を移動させる
+    var destinationTop  = getRandomInt(0, window.innerHeight - parseInt($('#target').css('height'), 10));
+    var destinationLeft = getRandomInt(0, window.innerWidth  - parseInt($('#target').css('width') , 10));
+    $('#target').animate({top: destinationTop, left: destinationLeft}, 'slow', null, arguments.callee);
+  };
   var init = function(){ // 物体の初期配置，タイマーの設置等
+    count = score = 0;
     var destinationTop  = getRandomInt(0, window.innerHeight - parseInt($('#target').css('height'), 10));
     var destinationLeft = getRandomInt(0, window.innerWidth  - parseInt($('#target').css('width') , 10));
     $('#target').css({top: destinationTop, left: destinationLeft});
     $('#timer').text('残り' + timeLimit + '秒');
     $('#timer').css({color: 'black'});
+    moveObject();
   };
     
   init();
   
   // 物体が押された場合，スコアを加算し，ランダムに再配置
   $('#target').click(function(){
-    score++;
-    $(this).css({display: 'none'});
-    var destinationTop  = getRandomInt(0, window.innerHeight - parseInt($(this).css('height'), 10));
-    var destinationLeft = getRandomInt(0, window.innerWidth  - parseInt($(this).css('width') , 10));
-    $(this).css({top: destinationTop, left: destinationLeft, display: 'block'});
+    console.log('score: ', ++score);
   });
   
   // 結果をアラートで出力
