4ffa7fd603b3f07ae614c4a4aec102e57a012423
佐々木悠 <y920ssk@gmail.com>
2015-07-18 04:49:41 +0000

    スコア表示の関数を作成

---
+++ end.js
@@ -0,0 +1,22 @@
+var point = 0;
+
+if(timer==0){
+  document.writeln('終了');
+   document.write(point);
+}
+   var startbutton =
+document.getElementById(“startbutton”);
+startbutton.addEventListener(“click”,
+function(){ alert(“Clicked!”}); 
+  
+}
+
+
+var DisplayResult =
+function(score){
+  if(window.confirm('ゲーム終了です\nあなたのスコアは'+score+'点です\n\nもう一度遊びますか？')){
+    
+    console.log('abc');
+  }
+  
+}
